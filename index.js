78f7a08bee7db04e51b9ab99cb1c1c3122ae73dd
Kaushal Kumar <Kaushalkumar297@gmail.com>
2021-05-12 15:57:58 +0530

    v1 Completed

---
+++ index.js
@@ -0,0 +1,49 @@
+class TypeWritter {
+    constructor(txtElement, words, wait = 3000){
+        this.txtElement = txtElement;
+        this.words = words;
+        this.txt = '';
+        this.wordIndex = 0;
+        this.wait = parseInt(wait, 10);
+        this.type();
+        this.isDeleting = false;
+    }
+
+    type(){
+        const current = this.wordIndex % this.words.length;
+        const fullTxt = this.words[current];
+
+        if(this.isDeleting){
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        }else{
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+        this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+        let typeSpeed = 300;
+        if(this.isDeleting){
+            typeSpeed /= 2;
+        }
+
+        if(!this.isDeleting && this.txt === fullTxt){
+            setTimeout(() => {
+                typeSpeed = this.wait;    
+                this.isDeleting = true;
+            }, 3000);
+        }else if(this.isDeleting && this.txt === ''){
+            this.isDeleting = false;
+            this.wordIndex++;
+            typeSpeed = 500;
+        }
+        setTimeout(()=> this.type(), 500);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init(){
+    const txtElement = document.querySelector('.txt-type');
+    const words= JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+
+    new TypeWritter(txtElement, words, wait);
+}
